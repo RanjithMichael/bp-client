cef6fefced851254b027e4213fe65fb1ee7c7174
RanjithMichael <branjithmichael@gmail.com>
2025-08-29 12:15:46 +0530

    updated api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
+const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -12,6 +12,7 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default API;
+
 
 
