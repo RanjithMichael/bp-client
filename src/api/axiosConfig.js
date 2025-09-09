e8f897eaabf46615756bba44b97b33c293b2a41d
RanjithMichael <branjithmichael@gmail.com>
2025-09-09 20:35:49 +0530

    updated api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bp-server-3.onrender.com/api", // replace with your backend URL
+  baseURL: "https://bp-server-3.onrender.com/api", 
 });
 
 // Add JWT token to requests if logged in
