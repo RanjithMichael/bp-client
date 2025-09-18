85ef454b12ea0f3394ed300a50d57863fcab184c
RanjithMichael <branjithmichael@gmail.com>
2025-09-18 20:31:35 +0530

    updated axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bp-server-3.onrender.com/api", 
+  baseURL: "https://bp-server-4.onrender.com/api", 
 });
 
 // Add JWT token to requests if logged in
