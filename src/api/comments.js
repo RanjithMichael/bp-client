61b1f890d69c055ed968594c1fab97b23ca2ab37
RanjithMichael <branjithmichael@gmail.com>
2026-03-11 21:44:53 +0530

    updated

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,11 +1,16 @@
+// src/api/commentApi.js
 import { get, post, remove } from "./axiosConfig.js";
 
 // COMMENTS
+
+// Add a new comment to a post
 export const addCommentToPost = (postId, text) =>
   post(`/comments/${postId}`, { text });
 
+// Get all comments for a post
 export const getCommentsByPost = (postId) =>
   get(`/comments/${postId}`);
 
+// Delete a comment from a post
 export const deleteCommentFromPost = (postId, commentId) =>
-  remove(`/comments/${postId}/comments/${commentId}`);
+  remove(`/comments/${postId}/${commentId}`);
